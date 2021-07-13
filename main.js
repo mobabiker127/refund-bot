e8415be3b8881a3a1f62097ed4bc49194e51ef14
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-13 13:07:07 +0100

    Refund9

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"]
 const prefix = '?';
 
 const fs = require('fs');
-const welcome = require('./welcome');
 
 client.commands = new Discord.Collection();
 
@@ -16,6 +15,19 @@ client.events = new Discord.Collection();
     require(`./handlers/${handler}`)(client, Discord); 
 })
 
-welcome(client)
+client.on('guildMemberAdd', (member) =>{
+
+    const channelId = member.guild.channels.cache.find(ch => ch.name === '\:wave:│join-logs')
+    const ruleID = member.guild.channels.cache.find(ch => ch.name === '\:pencil:・rules')
+    const guild = member.guild.id
+     
+    const welcome = guildMember.guild.channels.cache.send(`Welcome <@${guildMember.user.id}> to ${member.guild.name}! Make sure to read ${guildMember.guild.channels.cache.get(ruleID).toString()}.`)
+    const role = member.guild.roles.cache.find(role => role.name === 'Queued')
+    member.roles.add(role)
+    channelId.send(welcome)
+})
+
+
+
 
 client.login(process.env.token);
