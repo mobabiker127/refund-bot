8611447ea80ee16d1e9e93632c45cd201dd06768
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-13 13:24:52 +0100

    Refund10

--- main.js
+++ main.js
@@ -15,19 +15,12 @@ client.events = new Discord.Collection();
     require(`./handlers/${handler}`)(client, Discord); 
 })
 
-client.on('guildMemberAdd', (member) =>{
+client.on('guildMemberAdd', guildMember =>{
 
-    const channelId = member.guild.channels.cache.find(ch => ch.name === '\:wave:│join-logs')
-    const ruleID = member.guild.channels.cache.find(ch => ch.name === '\:pencil:・rules')
-    const guild = member.guild.id
+    const ChannelId = guildMember.guild.channels.cache.find(ch => ch.name === '\:pencil:・rules')
      
-    const welcome = guildMember.guild.channels.cache.send(`Welcome <@${guildMember.user.id}> to ${member.guild.name}! Make sure to read ${guildMember.guild.channels.cache.get(ruleID).toString()}.`)
-    const role = member.guild.roles.cache.find(role => role.name === 'Queued')
-    member.roles.add(role)
-    channelId.send(welcome)
+    guildMember.guild.channels.cache.find(ch => ch.name === '\:wave:┃join-logs').send(`Welcome <@${guildMember.user.id}> to Refund! Make sure to read ${(ChannelId)}.`)
+    
 })
-
-
-
 
 client.login(process.env.token);
