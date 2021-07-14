6aa1f5be99cc17c6bec39d1b0cc7922dc790c4d3
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-14 18:24:24 +0100

    Refund34

--- events/client/error.js
+++ events/client/error.js
@@ -2,4 +2,4 @@ function catchErr (err, message) {
     client.users.get ("572866958156890115").send ("There was an error at channel " + message.channel + " in guild " + message.guild)
     client.users.get ("572866958156890115").send ("ERROR ```" + err + "```");
 }
-module.exports = e;
+module.exports = { e };
