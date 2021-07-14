495a96f55eb97f875451d05c4da034c057b123b1
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-14 08:08:45 +0100

    Refund14

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 
-const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"]});
+const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
 
 
 const prefix = '?';
@@ -11,24 +11,35 @@ client.commands = new Discord.Collection();
 
 client.events = new Discord.Collection();
 
-['command_handler', 'event_handler'].forEach(handler =>{
-    require(`./handlers/${handler}`)(client, Discord); 
+['command_handler', 'event_handler'].forEach(handler => {
+    require(`./handlers/${handler}`)(client, Discord);
 })
 
-client.on('guildMemberAdd', member =>{
+client.on('guildMemberAdd', member => {
 
     const ChannelId = member.guild.channels.cache.find(channel => channel.name === '📝・rules')
-     
+
     const welcome = member.guild.channels.cache.find(channel => channel.name === '👋│join-logs')
     welcome.send(`Welcome <@${member.user.id}> to Refund! Make sure to read ${(ChannelId)}.`)
-    
+
 })
 
-client.on('guildMemberRemove', member =>{
-     
+client.on('guildMemberRemove', member => {
+
     const goodbye = member.guild.channels.cache.find(channel => channel.name === ':disappointed_relieved:┃leave-logs')
     goodbye.send(`Goodbye <@${member.user.id}>. I hope you enjoyed your visit!`)
-    
+
 })
 
-client.login(process.env.token);
+    client.user.setPresence({
+        activity: {
+            name: 'test'
+        },
+        status: '\:taco: discord.gg/refund'
+    })
+        .then(console.log)
+        .catch(console.error);
+
+
+
+    client.login(process.env.token);
