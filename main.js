63ada9d66e174d546ceb878d32717d352abda3e4
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-15 20:49:55 +0100

    Refund44

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"]
 //
 
 function catchErr (err, message) {
-
     client.users.get ("572866958156890115").send ("There was an error at channel " + message.channel + " in guild " + message.guild)
     client.users.get ("572866958156890115").send ("ERROR ```" + err + "```");
 }
