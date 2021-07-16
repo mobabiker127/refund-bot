3603d4be4848e74cb4f658a71a48c70deda7b018
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-16 17:57:32 +0100

    Refund55

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,6 +1,17 @@
 
 module.exports = () => {
-    
+
     console.log('Refund.')
+
+    let peopleIn = client.guild.members.cache;
+
+    client.user.setPresence({
+        activity: {
+            name: `${peopleIn} people.`,
+            type: "WATCHING"
+        },
+        status: 'idle'
+    })
+        .catch(console.error);
 
 }
--- main.js
+++ main.js
@@ -6,16 +6,6 @@ const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"]
 
 //
 
-let peopleIn = guild.members.cache;
-
-    client.user.setPresence({
-        activity: {
-            name: `${peopleIn} people.`,
-            type: "WATCHING"
-        },
-        status: 'idle'
-    })
-        .catch(console.error);
 
 const prefix = '?';
 
