c6904df4b93e7296ab5d04a29fdbb97a9a982702
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-16 21:23:27 +0100

    Refund84

--- events/client/ready.js
+++ events/client/ready.js
@@ -3,17 +3,6 @@ module.exports = (client, Discord, message, args) => {
 
         console.log('Refund.')
 
-        const peopleIn = client.guilds.get('813824410506100736').members.cache.filter(member => !member.user.bot).size;
-
-        client.user.setPresence({
-            activity: {
-                name: `${peopleIn} people.`,
-                type: "WATCHING"
-            },
-            status: 'idle'
-        })
-            .catch(console.error);
-
 
     })
 }
