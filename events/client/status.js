0009db6114f9bf8509b013b8d7fd20745d97bd80
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-16 20:50:36 +0100

    Refund78

--- events/client/status.js
+++ events/client/status.js
@@ -1,16 +1,14 @@
-module.exports = {
-    execute(client, Discord, message, args){
+module.exports = (client, Discord, message, args) => {
 
-        const peopleIn = client.guilds.get('').members.cache.filter(member => !member.user.bot).size;
+    const peopleIn = client.guilds.get('').members.cache.filter(member => !member.user.bot).size;
 
-        client.user.setPresence({
-            activity: {
-                name: `${peopleIn} people.`,
-                type: "WATCHING"
-            },
-            status: 'idle'
-        })
-            .catch(console.error);
-            
-    }
+    client.user.setPresence({
+        activity: {
+            name: `${peopleIn} people.`,
+            type: "WATCHING"
+        },
+        status: 'idle'
+    })
+        .catch(console.error);
+
 }
