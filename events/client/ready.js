f761d918d7cb987a3fc091b2723179310d5ceca2
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-16 19:11:48 +0100

    Refund68

--- events/client/ready.js
+++ events/client/ready.js
@@ -3,7 +3,7 @@ module.exports = {
     execute(client, message, args, Discord) {
     console.log('Refund.')
 
-    const peopleIn = client.guilds.cache.size
+    const peopleIn = client.guilds.get('813824410506100736').members.cache.filter(member => !member.user.bot).size;
 
     client.user.setPresence({
         activity: {
