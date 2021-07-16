ec793879389f4fcd989567bd9b05263d7ea6dbbc
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-16 16:47:45 +0100

    Refund50

--- commands/mute.js
+++ commands/mute.js
@@ -1,37 +1,39 @@
 const ms = require('ms');
+const main = require("/Users/mobabiker/Documents/GitHub/refundbot/main.js");
 
 module.exports = {
     name: 'mute',
     description: "Mutes a member in the server.",
     permissions: ["MANAGE_MESSAGES"],
-    execute(client, message, args, Discord){
+    execute(client, message, args, Discord) {
 
-        
+
         const target = message.mentions.users.first();
-        if(target){
+        if (target) {
 
             let muteRole = message.guild.roles.cache.find(role => role.name === 'Muted');
 
             let memberTarget = message.guild.members.cache.get(target.id);
 
-            if(!args[1]){
+            if (!args[1]) {
                 memberTarget.roles.add(muteRole.id);
                 message.channel.send(`<@${memberTarget.user.id}> has been muted indefinitely.`);
-                return 
+                return
             }
             memberTarget.roles.add(muteRole.id);
             message.channel.send(`<@${memberTarget.user.id}> has been muted for ${ms(ms(args[1]))}`);
 
-            setTimeout(function(){
+            setTimeout(function () {
                 memberTarget.roles.remove(muteRole.id);
             }, ms(args[1]));
-            
+
         }
-        else{
+        else {
             message.channel.send('Not a valid user.');
         }
-        }
-        
-
-        
     }
+
+
+
+}
+    
