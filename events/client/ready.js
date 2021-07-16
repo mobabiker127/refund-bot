4e84ae1085941b156b2f51b74014316821bcaab9
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-16 22:15:27 +0100

    Refund91

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,23 +1,6 @@
 
-module.exports = (client, Discord) => {
-    client.once('ready', () => {
-
+module.exports = () => {
         console.log('Refund.')
-
-
-        const peopleIn = client.guilds.get('813824410506100736').members.cache.filter(member => !member.user.bot).size;
-
-        client.user.setPresence({
-            activity: {
-                name: `${peopleIn} people.`,
-                type: "WATCHING"
-            },
-            status: 'idle'
-        })
-            .catch(console.error);
-
-    })
-};
-
+}
 
 
---
+++ events/client/status.js
@@ -0,0 +1,14 @@
+client.once('ready', () => {
+
+    const peopleIn = client.guilds.get('813824410506100736').members.cache.filter(member => !member.user.bot).size;
+
+    client.user.setPresence({
+        activity: {
+            name: `${peopleIn} people.`,
+            type: "WATCHING"
+        },
+        status: 'idle'
+    })
+        .catch(console.error);
+
+});
