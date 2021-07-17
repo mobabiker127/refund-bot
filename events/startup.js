7a15d3d12cfe9720d352404846d9af3e3fa32cb2
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-17 15:04:01 +0100

    Refund135

--- events/startup.js
+++ events/startup.js
@@ -1,18 +1,21 @@
-client.on("ready", function(){
-    console.log(`Refund.`);
+module.exports = (client, Discord) => {
 
-    const peopleIn = client.guilds.cache.get('813824410506100736').members.cache.filter(member => !member.user.bot).size;
+    client.on("ready", function () {
+        console.log(`Refund.`);
 
-    const arrayOfStatus = [
-        `${peopleIn} people.`,
-        `discord.gg/refund 🌮`
-    ];
-    let index = 0;
-    setInterval(() => {
-        if(index === arrayOfStatus.length) index = 0;
-        const status = arrayOfStatus[index];
-        console.log(status);
-        client.user.setActivity(status, { type: "WATCHING" }).catch(console.error)
-        index++;
-    }, 6000)
-})
+        const peopleIn = client.guilds.cache.get('813824410506100736').members.cache.filter(member => !member.user.bot).size;
+
+        const arrayOfStatus = [
+            `${peopleIn} people.`,
+            `discord.gg/refund 🌮`
+        ];
+        let index = 0;
+        setInterval(() => {
+            if (index === arrayOfStatus.length) index = 0;
+            const status = arrayOfStatus[index];
+            console.log(status);
+            client.user.setActivity(status, { type: "WATCHING" }).catch(console.error)
+            index++;
+        }, 6000)
+    })
+}
