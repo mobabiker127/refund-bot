c4845c763025eb20a1d76172733e978397cbc4ea
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-17 09:05:46 +0100

    Refund113

--- commands/status.js
+++ commands/status.js
@@ -3,7 +3,17 @@ module.exports = {
     description: "sets bot statud",
     permissions: ["ADMINISTRATOR"],
     async execute(client, Discord) {
-        
-        client.user.setActivity('Test')
+
+        const peopleIn = client.guilds.get('813824410506100736').members.cache.filter(member => !member.user.bot).size;
+
+        client.user.setPresence({
+            activity: {
+                name: `${peopleIn} people.`,
+                type: "WATCHING"
+            },
+            status: 'idle'
+        })
+            .catch(console.error);
+
     }
 }
