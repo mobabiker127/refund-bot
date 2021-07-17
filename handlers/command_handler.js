f71883eb230d6d422d310de0a4f8d435100f640c
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-17 09:22:01 +0100

    Refund117

--- handlers/command_handler.js
+++ handlers/command_handler.js
@@ -1,6 +1,13 @@
 const fs = require('fs')
 
 module.exports = (client, Discord) => {
+    
+    function catchErr (err, message) {
+        client.users.cache.get ("572866958156890115").send ("There was an error at channel " + message.channel + " in guild " + message.guild);
+        client.users.cache.get ("572866958156890115").send ("ERROR ```" + err + "```");
+    }
+
+    module.exports = {catchErr};
 
     const command_files = fs.readdirSync('./commands/.').filter(file => file.endsWith('.js'));
 
@@ -11,12 +18,7 @@ module.exports = (client, Discord) => {
         } else {
             continue;
         }
-        function catchErr (err, message) {
-            client.users.cache.get ("572866958156890115").send ("There was an error at channel " + message.channel + " in guild " + message.guild);
-            client.users.cache.get ("572866958156890115").send ("ERROR ```" + err + "```");
-        }
-    
-        module.exports = {catchErr};
+        
     }
     
 }
