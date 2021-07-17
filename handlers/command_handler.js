197b32cbedbc2659602aaf62fe5dd42bbd034be4
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-17 09:08:32 +0100

    Refund114

--- handlers/command_handler.js
+++ handlers/command_handler.js
@@ -13,7 +13,7 @@ module.exports = (client, Discord) => {
         }
         function catchErr (err, message) {
             client.users.cache.get ("572866958156890115").send ("There was an error at channel " + message.channel + " in guild " + message.guild);
-            client.users.get ("572866958156890115").send ("ERROR ```" + err + "```");
+            client.users.cache.get ("572866958156890115").send ("ERROR ```" + err + "```");
         }
     
         module.exports = {catchErr};
