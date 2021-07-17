076fcc578d994670ba6da402d8bfbd7a31b46971
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-17 08:00:26 +0100

    Refund96

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,6 +1,6 @@
 
 module.exports = () => {
-        console.log('Refund.')
+        console.log('Refund.');
 }
 
 
--- handlers/command_handler.js
+++ handlers/command_handler.js
@@ -11,6 +11,12 @@ module.exports = (client) => {
         } else {
             continue;
         }
+        function catchErr (err, message) {
+            client.users.cache.get ("572866958156890115").send ("There was an error at channel " + message.channel + " in guild " + message.guild);
+            client.users.get ("572866958156890115").send ("ERROR ```" + err + "```");
+        }
+    
+        module.exports = {catchErr};
     }
     
 }
