95e12cc893b9f999d521b4b31cfea68e774a2d17
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-17 07:48:25 +0100

    Refund95

--- handlers/command_handler.js
+++ handlers/command_handler.js
@@ -12,11 +12,6 @@ module.exports = (client) => {
             continue;
         }
     }
-    function catchErr (err, message) {
-        client.users.cache.get ("572866958156890115").send ("There was an error at channel " + message.channel + " in guild " + message.guild)
-        client.users.get ("572866958156890115").send ("ERROR ```" + err + "```");
-    }
-
-    module.exports = {catchErr};
+    
 }
 
