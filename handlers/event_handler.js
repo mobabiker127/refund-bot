ec7e38fb01ff3a410d8ad31d043219f05cc6a656
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-17 13:38:49 +0100

    Refund121

--- handlers/event_handler.js
+++ handlers/event_handler.js
@@ -1,11 +1,13 @@
+// new event handler
+
 const fs = require('fs')
 
 module.exports = (client, Discord) =>{
-    fs.readdirSync(`./events/${dirs}`).forEach(file => {
-        const events = fs.readdirSync(`./events/${dirs}`).filter((files) => files.endsWith('.js'));
+    fs.readdirSync('./events/').forEach(file => {
+        const events = fs.readdirSync('./events/').filter((files) => files.endsWith('.js'));
 
         for (let files of events){
-            let get = require(`../events/${dirs}/${file}`)
+            let get = require(`../events/${files }`)
 
             if(get.name){
                 client.events.set(get.name, get)
@@ -16,4 +18,3 @@ module.exports = (client, Discord) =>{
         }
     })
 }
-
