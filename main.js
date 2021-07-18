807c155deb6cc3c803456adb5316981568b40209
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-18 12:40:26 +0100

    Refund166

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ try {
 
     const Discord = require('discord.js');
 
-    const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
+    const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION", "USER"] });
 
     //
 
@@ -65,44 +65,19 @@ try {
 
     }
     )
-    let guildID = "856174227814875147";
-    let channelID = "857636392564424725";
-    let emojiID = "🍴";
-    let roleID = "856234924465586197";
-
-
-    client.on("ready", async () => {
-
-        let guild = client.guilds.cache.find(guild => guild.id == guildID);
-        let channel = await guild.channels.cache.find(ch => ch.id == channelID)
-
-        // You can set any limit you want, for performance I used a low number
-        channel.messages.fetch({ limit: 10 })
-            .then(async messages => {
-                messages.forEach(async message => {
-                
-                    if (message.partial) await message.fetch();
-                    if (!message.guild) return;
-
-                    for (let reactionObj of message.reactions.cache) {
-                        for (let reaction of reactionObj) {
-                            if (typeof reaction == "string") continue;
-                            if (reaction.emoji.name != emojiID) continue;
-                            reaction.users.fetch()
-                                .then(async users => {
-                                    users.forEach(async user => {
-                                        if (user.client) return;
-                                        console.log("Adding role")
-                                        await reaction.message.guild.members.cache.get(user.id).roles.add(roleID)
-                                    })
-                                })
-                        }
-                    }
-
-                });
-            })
-
-    });
+    client.on(
+        'messageReactionAdd',
+        async (potentialPartialReaction, potentialPartialUser) => {
+          try {
+            const reaction = await potentialPartialReaction.fetch();
+            const user = await potentialPartialUser.fetch();
+          } catch (err) {
+            console.log(err);
+          }
+      
+          // can use reaction and user here!
+      });
+    
 
 
     client.login(process.env.token);
