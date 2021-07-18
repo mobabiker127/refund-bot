db6ecc68c35e4378bd7ce14b67faea507b6daa49
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-18 12:45:12 +0100

    Refund167

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ try {
 
     const Discord = require('discord.js');
 
-    const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION", "USER"] });
+    const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
 
     //
 
@@ -77,6 +77,23 @@ try {
       
           // can use reaction and user here!
       });
+      client.on('messageReactionAdd', async (reaction, user) => {
+        // When a reaction is received, check if the structure is partial
+        if (reaction.partial) {
+            // If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
+            try {
+                await reaction.fetch();
+            } catch (error) {
+                console.error('Something went wrong when fetching the message: ', error);
+                // Return as `reaction.message.author` may be undefined/null
+                return;
+            }
+        }
+        // Now the message has been cached and is fully available
+        console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
+        // The reaction is now also fully available and the properties will be reflected accurately:
+        console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
+    });
     
 
 
