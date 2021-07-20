6fc37da4eaeb8a4e738be7a76527355293e9837e
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-20 19:54:06 +0100

    Refund196

--- commands/lock.js
+++ commands/lock.js
@@ -11,6 +11,7 @@ module.exports = {
             if (!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send('You do not have permissions to run this command.')
 
             const role = message.guild.roles.cache.find(role => role.name === "Customer");
+            
             let lockChannel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]);
             if (!lockChannel) lockChannel = message.channel;
 
