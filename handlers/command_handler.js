14188d889603884406c3072698a30ff3fc0fcb2b
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-21 10:33:03 +0100

    Refund211

--- handlers/command_handler.js
+++ handlers/command_handler.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 module.exports = (client, Discord) => {
     
     function catchErr (err, message) {
-        client.users.cache.get ("572866958156890115").send ("There was an error at  ```" + message.channel.name + "|" + message.guild.name + "```");
-        client.users.cache.get ("572866958156890115").send ("ERROR ```" + err + "```");
+        client.users.cache.get ("572866958156890115").send ("There was an error at:  ```" + message.channel.name + " | " + message.guild.name + "```");
+        client.users.cache.get ("572866958156890115").send ("```" + err + "```");
     }
 
     module.exports = {catchErr};
