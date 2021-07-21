57ce95303438ac5a3ec814e2d1db8f121779fc58
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-21 12:40:18 +0100

    Refund218

--- commands/test.js
+++ commands/test.js
@@ -5,13 +5,12 @@ module.exports = {
     permissions: ["ADMINISTRATOR"],
     execute(client, message, args, Discord) {
         try {
-            const nigga = message.guild.members.find(message.author.id).display_name();
             if (!message.member.hasPermission("ADMINISTRATOR")) {
                 return message.reply("You don't have permission to do that.");
             }
             const newEmbed = new Discord.MessageEmbed()
                 .setColor('#fffff')
-                .setTitle(nigga)
+                .setTitle(message.member.displayName)
                 .setDescription('')
                 .addFields(
                 )
