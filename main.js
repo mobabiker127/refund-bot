64137741d3ac2d442417d2ea17ae1dcb81afca37
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-22 16:59:05 +0100

    Refund226

--- main.js
+++ main.js
@@ -1,148 +1,142 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
+
+const mongoose = require('mongoose');
+
+//
+
+const ms = require('ms');
+const prefix = '?';
+
+const fs = require('fs');
+
+const { runInContext } = require('vm');
+
+client.commands = new Discord.Collection();
+
+client.events = new Discord.Collection();
+
+module.exports = client;
+
+['command_handler', 'event_handler'].forEach(handler => {
+    require(`./handlers/${handler}`)(client, Discord);
+
+})
+
+mongoose.connect(process.env.MONGODB_SRV, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}).then(() => {
+    console.log('Connected to the database.')
+}).catch((err) => {
+    console.log(err);
+});
 
 
+client.on('guildMemberAdd', member => {
 
-try {
+    const ChannelId = member.guild.channels.cache.find(channel => channel.name === '📜│server-rules')
 
-    const Discord = require('discord.js');
+    const welcome = member.guild.channels.cache.find(channel => channel.name === '👋│join-logs')
+    welcome.send(`Welcome <@${member.user.id}> to Refund! Make sure to read ${(ChannelId)}.`)
+    member.roles.add(member.guild.roles.cache.find(role => role.name === "Queued"));
 
-    const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
 
-    const mongoose = require('mongoose');
+})
 
-    //
+client.on('guildMemberRemove', member => {
 
-    const ms = require('ms');
-    const prefix = '?';
+    const goodbye = member.guild.channels.cache.find(channel => channel.name === '😥│leave-logs')
+    goodbye.send(`Goodbye ${member.user.tag}. I hope you enjoyed your visit!`)
 
-    const fs = require('fs');
+})
 
-    const { runInContext } = require('vm');
 
-    client.commands = new Discord.Collection();
+client.on("ready", function () {
+    console.log(`Refund.`);
 
-    client.events = new Discord.Collection();
+    const peopleIn = client.guilds.cache.get('813824410506100736').members.cache.filter(member => !member.user.bot).size;
 
-    module.exports = client;
-
-    ['command_handler', 'event_handler'].forEach(handler => {
-        require(`./handlers/${handler}`)(client, Discord);
-
+    client.user.setPresence({
+        activity: {
+            name: `${peopleIn} people.`,
+            type: "WATCHING"
+        },
+        status: 'idle'
     })
+        .catch(console.error);
 
-    mongoose.connect(process.env.MONGODB_SRV, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false
-    }).then(()=>{
-        console.log('Connected to the database.')
-    }).catch((err) =>{
-        console.log(err);
-    });
+}
+)
+client.on('messageReactionAdd', async (reaction, user) => {
 
+    const customerRole = reaction.message.guild.roles.cache.find(r => r.name === "Customer");
 
-    client.on('guildMemberAdd', member => {
+    if (reaction.message.partial) await reaction.message.fetch();
+    if (reaction.partial) await reaction.fetch();
+    if (user.bot) return;
+    if (!reaction.message.guild) return;
 
-        const ChannelId = member.guild.channels.cache.find(channel => channel.name === '📜│server-rules')
-
-        const welcome = member.guild.channels.cache.find(channel => channel.name === '👋│join-logs')
-        welcome.send(`Welcome <@${member.user.id}> to Refund! Make sure to read ${(ChannelId)}.`)
-        member.roles.add(member.guild.roles.cache.find(role => role.name === "Queued"));
-
-
-    })
-
-    client.on('guildMemberRemove', member => {
-
-        const goodbye = member.guild.channels.cache.find(channel => channel.name === '😥│leave-logs')
-        goodbye.send(`Goodbye ${member.user.tag}. I hope you enjoyed your visit!`)
-
-    })
-
-
-    client.on("ready", function () {
-        console.log(`Refund.`);
-
-        const peopleIn = client.guilds.cache.get('813824410506100736').members.cache.filter(member => !member.user.bot).size;
-
-        client.user.setPresence({
-            activity: {
-                name: `${peopleIn} people.`,
-                type: "WATCHING"
-            },
-            status: 'idle'
-        })
-            .catch(console.error);
-
-    }
-    )
-    client.on('messageReactionAdd', async (reaction, user) =>{
-
-        const customerRole = reaction.message.guild.roles.cache.find(r => r.name === "Customer");
-    
-        if (reaction.message.partial) await reaction.message.fetch();
-        if (reaction.partial) await reaction.fetch();
-        if (user.bot) return;
-        if(!reaction.message.guild) return;
-    
-        if(reaction.message.channel.id === '813826725112447077'){
-            if(reaction.message.id === '866372822796206080'){
-            if(reaction.emoji.name === '🍴'){
+    if (reaction.message.channel.id === '813826725112447077') {
+        if (reaction.message.id === '866372822796206080') {
+            if (reaction.emoji.name === '🍴') {
                 await reaction.message.guild.members.cache.get(user.id).roles.add(customerRole).catch(console.error);
-            }  }         
-        } else{
-            return;
+            }
         }
-    
-    });
+    } else {
+        return;
+    }
 
-    client.on('messageReactionAdd', async (reaction, user) =>{
+});
 
-        const gamesRole = reaction.message.guild.roles.cache.find(r => r.name === "Games");
-    
-        if (reaction.message.partial) await reaction.message.fetch();
-        if (reaction.partial) await reaction.fetch();
-        if (user.bot) return;
-        if(!reaction.message.guild) return;
-    
-        if(reaction.message.channel.id === '864958460427501578'){
-            if(reaction.message.id === '866374386888998933'){
-            if(reaction.emoji.name === '🕹️'){
+client.on('messageReactionAdd', async (reaction, user) => {
+
+    const gamesRole = reaction.message.guild.roles.cache.find(r => r.name === "Games");
+
+    if (reaction.message.partial) await reaction.message.fetch();
+    if (reaction.partial) await reaction.fetch();
+    if (user.bot) return;
+    if (!reaction.message.guild) return;
+
+    if (reaction.message.channel.id === '864958460427501578') {
+        if (reaction.message.id === '866374386888998933') {
+            if (reaction.emoji.name === '🕹️') {
                 await reaction.message.guild.members.cache.get(user.id).roles.add(gamesRole).catch(console.error);
-            }  }         
-        } else{
-            return;
+            }
         }
-    
-    });
+    } else {
+        return;
+    }
 
-    client.on('messageReactionAdd', async (reaction, user) =>{
+});
 
-        const moviesRole = reaction.message.guild.roles.cache.find(r => r.name === "Movies");
-    
-        if (reaction.message.partial) await reaction.message.fetch();
-        if (reaction.partial) await reaction.fetch();
-        if (user.bot) return;
-        if(!reaction.message.guild) return;
-    
-        if(reaction.message.channel.id === '864958460427501578'){
-            if(reaction.message.id === '866374372272766987'){
-            if(reaction.emoji.name === '🎥'){
+client.on('messageReactionAdd', async (reaction, user) => {
+
+    const moviesRole = reaction.message.guild.roles.cache.find(r => r.name === "Movies");
+
+    if (reaction.message.partial) await reaction.message.fetch();
+    if (reaction.partial) await reaction.fetch();
+    if (user.bot) return;
+    if (!reaction.message.guild) return;
+
+    if (reaction.message.channel.id === '864958460427501578') {
+        if (reaction.message.id === '866374372272766987') {
+            if (reaction.emoji.name === '🎥') {
                 await reaction.message.guild.members.cache.get(user.id).roles.add(moviesRole).catch(console.error);
-            }  }         
-        } else{
-            return;
+            }
         }
-    
-    });
-    
+    } else {
+        return;
+    }
+
+});
 
 
-    client.login(process.env.token);
-}
 
-catch (err) {
-    catchErr(err, message);
-}
+client.login(process.env.token);
+
 
 
 // 
