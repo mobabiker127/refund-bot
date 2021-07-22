f1ca4af0e7724e6365c5c8458c925f5a2b5b8af2
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-22 17:33:06 +0100

    Refund229

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
 
+const dotenv = require('dotenv').config();
+
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
 
 const mongoose = require('mongoose');
