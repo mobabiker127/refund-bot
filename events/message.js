62ea967205d3f7c9b62b2c7f2536c45301a6d6f7
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-23 19:35:54 +0100

    Refund257

--- events/message.js
+++ events/message.js
@@ -1,86 +1,60 @@
-module.exports = (Discord, client, message) =>{
-    const prefix = '?';
-    const cooldowns = new Map();
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
+module.exports = (Discord, client, message) => {
+  const prefix = '?';
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const cmd = args.shift().toLowerCase();
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const cmd = args.shift().toLowerCase();
 
-    const command = client.commands.get(cmd);
-    if(command) command.execute(client, message, args, Discord);
-    if(!command) return;
+  const command = client.commands.get(cmd);
+  if (command) command.execute(client, message, args, Discord);
+  if (!command) return;
 
-    if(!cooldowns.has(command.name)){
-      cooldowns.set(command.name, new Discord.Collection());
-  }
-
-  const current_time = Date.now();
-  const time_stamps = cooldowns.get(command.name);
-  const cooldown_amount = (command.cooldown) * 1000;
-
-  if(time_stamps.has(message.author.id)){
-      const expiration_time = time_stamps.get(message.author.id) + cooldown_amount;
-      if(current_time < expiration_time){
-          const time_left = (expiration_time - current_time) / 1000;
-
-          return message.reply(`You cannot use this command for ${time_left.toFixed(0)} seconds.`);
-      }
-  }
-
-  time_stamps.set(message.author.id, current_time);
-  setTimeout(() => time_stamps.delete(message.author.id), cooldown_amount);
-
-  try{
-    command.execute(message, args, cmd, client, Discord);
-} catch (err){
-    message.reply("There was an error trying to execute this command!");
-    console.log(err);
 }
 
-    const validPermissions = [
-        "CREATE_INSTANT_INVITE",
-        "KICK_MEMBERS",
-        "BAN_MEMBERS",
-        "ADMINISTRATOR",
-        "MANAGE_CHANNELS",
-        "MANAGE_GUILD",
-        "ADD_REACTIONS",
-        "VIEW_AUDIT_LOG",
-        "PRIORITY_SPEAKER",
-        "STREAM",
-        "VIEW_CHANNEL",
-        "SEND_MESSAGES",
-        "SEND_TTS_MESSAGES",
-        "MANAGE_MESSAGES",
-        "EMBED_LINKS",
-        "ATTACH_FILES",
-        "READ_MESSAGE_HISTORY",
-        "MENTION_EVERYONE",
-        "USE_EXTERNAL_EMOJIS",
-        "VIEW_GUILD_INSIGHTS",
-        "CONNECT",
-        "SPEAK",
-        "MUTE_MEMBERS",
-        "DEAFEN_MEMBERS",
-        "MOVE_MEMBERS",
-        "USE_VAD",
-        "CHANGE_NICKNAME",
-        "MANAGE_NICKNAMES",
-        "MANAGE_ROLES",
-        "MANAGE_WEBHOOKS",
-        "MANAGE_EMOJIS",
-      ]
-    
-      if(command.permissions.length){
-        let invalidPerms = []
-        for(const perm of command.permissions){
-          if(!validPermissions.includes(perm)){
-            return console.log(`Invalid Permissions ${perm}`);
-          }
-          if(!message.member.hasPermission(perm)){
-            invalidPerms.push(perm);
-          }
-        }
-      }
-    
+const validPermissions = [
+  "CREATE_INSTANT_INVITE",
+  "KICK_MEMBERS",
+  "BAN_MEMBERS",
+  "ADMINISTRATOR",
+  "MANAGE_CHANNELS",
+  "MANAGE_GUILD",
+  "ADD_REACTIONS",
+  "VIEW_AUDIT_LOG",
+  "PRIORITY_SPEAKER",
+  "STREAM",
+  "VIEW_CHANNEL",
+  "SEND_MESSAGES",
+  "SEND_TTS_MESSAGES",
+  "MANAGE_MESSAGES",
+  "EMBED_LINKS",
+  "ATTACH_FILES",
+  "READ_MESSAGE_HISTORY",
+  "MENTION_EVERYONE",
+  "USE_EXTERNAL_EMOJIS",
+  "VIEW_GUILD_INSIGHTS",
+  "CONNECT",
+  "SPEAK",
+  "MUTE_MEMBERS",
+  "DEAFEN_MEMBERS",
+  "MOVE_MEMBERS",
+  "USE_VAD",
+  "CHANGE_NICKNAME",
+  "MANAGE_NICKNAMES",
+  "MANAGE_ROLES",
+  "MANAGE_WEBHOOKS",
+  "MANAGE_EMOJIS",
+]
+
+if (command.permissions.length) {
+  let invalidPerms = []
+  for (const perm of command.permissions) {
+    if (!validPermissions.includes(perm)) {
+      return console.log(`Invalid Permissions ${perm}`);
+    }
+    if (!message.member.hasPermission(perm)) {
+      invalidPerms.push(perm);
+    }
+  }
 }
+
+
