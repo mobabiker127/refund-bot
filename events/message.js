b8c5e8b2dc07e6a44f8c69d2ed6626730598ef8b
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-23 11:26:02 +0100

    Refund240

--- events/message.js
+++ events/message.js
@@ -1,5 +1,6 @@
 module.exports = (Discord, client, message) =>{
     const prefix = '?';
+    const cooldowns = new Map();
     if(!message.content.startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split(/ +/);
@@ -8,6 +9,26 @@ module.exports = (Discord, client, message) =>{
     const command = client.commands.get(cmd);
     if(command) command.execute(client, message, args, Discord);
     if(!command) return;
+
+    if(!cooldowns.has(command.name)){
+      cooldowns.set(command.name, new Discord.Collection());
+  }
+
+  const current_time = Date.now();
+  const time_stamps = cooldowns.get(command.name);
+  const cooldown_ammount = (command.cooldown) * 1000;
+
+  if(time_stamps.has(message.author.id)){
+      const expiration_time = time_stamps.get (message.author.id) + cooldown_amount;
+      if(current_time < expiration_time){
+          const time_left = (expiration_time - current_time) / 1000;
+
+          return message.reply(`You cannot use this command for ${time_left.toFixed(0)} seconds.`)
+      }
+  }
+
+  time_stamps.set(message.author.id, current_time);
+  setTimeout(() => time_stamps.delete(message.author.id), cooldown_amount);
 
     const validPermissions = [
         "CREATE_INSTANT_INVITE",
--- handlers/command_handler.js
+++ handlers/command_handler.js
@@ -1,7 +1,5 @@
 const fs = require('fs')
 
-const cooldowns = new Map();
-
 module.exports = (message, client, Discord) => {
     
     function catchErr (err, message) {
@@ -10,26 +8,6 @@ module.exports = (message, client, Discord) => {
     }
 
     module.exports = {catchErr};
-
-    if(!cooldowns.has(command.name)){
-        cooldowns.set(command.name, new Discord.Collection());
-    }
-
-    const current_time = Date.now();
-    const time_stamps = cooldowns.get(command.name);
-    const cooldown_ammount = (command.cooldown) * 1000;
-
-    if(time_stamps.has(message.author.id)){
-        const expiration_time = time_stamps.get (message.author.id) + cooldown_amount;
-        if(current_time < expiration_time){
-            const time_left = (expiration_time - current_time) / 1000;
-
-            return message.reply(`You cannot use this command for ${time_left.toFixed(0)} seconds.`)
-        }
-    }
-
-    time_stamps.set(message.author.id, current_time);
-    setTimeout(() => time_stamps.delete(message.author.id), cooldown_amount);
 
     const command_files = fs.readdirSync('./commands/.').filter(file => file.endsWith('.js'));
 
