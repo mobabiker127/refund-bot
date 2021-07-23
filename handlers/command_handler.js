9053734af81d3c08c3ffaa3e4c5c6772a173ff04
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-23 11:18:50 +0100

    Refund238

--- handlers/command_handler.js
+++ handlers/command_handler.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 
 const cooldowns = new Map();
 
-module.exports = (client, Discord) => {
+module.exports = (Discord, client, message) => {
     
     function catchErr (err, message) {
         client.users.cache.get ("572866958156890115").send ("There was an error at:  ```" + message.channel.name + " | " + message.guild.name + "```");
