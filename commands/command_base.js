8715e34fa99041fec9f6482c8df30b3b5ee8240a
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-24 21:14:36 +0100

    Refund313

--- commands/command_base.js
+++ commands/command_base.js
@@ -1,5 +1,5 @@
 const { prefix } = require('../package.json')
-const moment = require('humanize-duration');
+const Duration = require('humanize-duration')
 
 const validatePermissions = (permissions) => {
     const validPermissions = [
@@ -95,10 +95,10 @@ module.exports = (client, commandOptions) => {
                     }
 
                 }
-                const timeLeft = moment(cooldown - Date.now())
+                const timeLeft = Duration(cooldown - Date.now())
                 let cooldownString  = ''
                 if (cooldown > 0 && recentlyRan.includes(cooldownString)) {
-                    message.reply(`You must wait ${timeLeft(0)} seconds before using this command again.`)
+                    message.reply(`You must wait ${timeLeft.toFixed(0)} seconds before using this command again.`)
                     return
                 }
 
