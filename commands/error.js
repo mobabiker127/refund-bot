c4df373e1af421725168bd3932b12afbcbe155b6
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-24 11:57:37 +0100

    Refund285

--- commands/error.js
+++ commands/error.js
@@ -1,9 +1,9 @@
 module.exports = (client, Discord) => {
-
-    function catchErr(err, message) {
-        client.users.cache.get("572866958156890115").send("There was an error at:  ```" + message.channel.name + " | " + message.guild.name + "```");
-        client.users.cache.get("572866958156890115").send("```" + err + "```");
+    
+    function catchErr (err, message) {
+        client.users.cache.get ("572866958156890115").send ("There was an error at:  ```" + message.channel.name + " | " + message.guild.name + "```");
+        client.users.cache.get ("572866958156890115").send ("```" + err + "```");
     }
-}
 
-module.exports = { catchErr };
+    module.exports = {catchErr};
+}
