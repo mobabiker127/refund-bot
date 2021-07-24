82d063a2ffb939a792c3c5f0b0aa8b4aea3c0bd2
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-24 11:05:59 +0100

    Refund273

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,4 +1,4 @@
-const { catchErr } = require('./commands/command_base.js');
+const { catchErr } = require('../command_base.js');
 module.exports = {
     name: 'av',
     description: "displays a users avatar in the server",
