ef28c39c30e83194bdb3983ac2add4c3d3fafa60
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-24 20:41:52 +0100

    Refund307

--- commands/suggestion.js
+++ commands/suggestion.js
@@ -2,8 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
 module.exports = {
     commands: ['suggest', 'suggestion'],
-    permissions: ['ADMINISTRATOR'],
     cooldown: 60,
+    permissions: ['ADMINISTRATOR'],
     callback: (message, arguments, text) => {
 
         const channel = message.guild.channels.cache.find(c => c.name === '📫・suggestions');
