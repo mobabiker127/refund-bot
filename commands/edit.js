d1fc3c3ea94e07d87674a7471ecdfd684c662da2
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-26 14:31:13 +0100

    Refund355

---
+++ commands/edit.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
+module.exports = {
+    commands: ['edit'],
+    minArgs: 0,
+    maxArgs: 1,
+    permissions: ['ADMINISTRATOR'],
+    async callback(message, arguments, text) {
+
+        const gamesEmoji = '🕹️';
+
+        let embed = new Discord.MessageEmbed()
+            .setColor('#56F307')
+            .setTitle('React to this to get the Games role.')
+            .setDescription('You will be notified whenever there is a game night!\n\n'
+                + `${gamesEmoji} Games role\n`);
+        message.channel.messages.fetch('866369346473164822')
+        const embededit = msg.first()
+        embededit.edit(embed);
+
+    }
+}
