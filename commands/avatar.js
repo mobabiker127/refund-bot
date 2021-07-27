d5333163d586b951739a48f48a953bf4edbc4c1c
Blactose <87344571+Blactose@users.noreply.github.com>
2021-07-27 12:23:36 +0100

    Refund365

--- commands/avatar.js
+++ commands/avatar.js
@@ -7,18 +7,22 @@ module.exports = {
     maxArgs: 1,
     callback: (message, arguments, text) => {
 
+        const messages = ["Looking sexy as always", "What the fuck is that", "Bro I'd smash", "Garbage"]
+
+        const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+
         const embed = new Discord.MessageEmbed()
 
         if (!message.mentions.users.first()) {
             embed.setTitle("Your Avatar:")
-            embed.setDescription("Your avatar is looking sexy.")
+            embed.setDescription(randomMessage)
             embed.setColor("#00000")
             embed.setImage(message.author.displayAvatarURL({ size: 4096, dynamic: true }))
             return message.channel.send(embed)
         } else {
             const user = message.mentions.users.first()
             embed.setTitle(`${user.tag}'s Avatar`)
-            embed.setDescription(`They should really change it. `)
+            embed.setDescription(randomMessage)
             embed.setColor("#00000")
             embed.setImage(user.displayAvatarURL({ size: 4096, dynamic: true }))
             return message.channel.send(embed)
