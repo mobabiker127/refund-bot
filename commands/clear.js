51557405f03cf1d92ef0a9ddf8572fcced18c947
Blactose <87344571+Blactose@users.noreply.github.com>
2022-01-20 16:30:50 +0000

    Refund401

--- commands/clear.js
+++ commands/clear.js
@@ -6,29 +6,19 @@ module.exports = {
     minArgs: 0,
     maxArgs: 1,
     permissions: ['MANAGE_MESSAGES'],
-    async callback(message, arguments, text) {
+    async execute(message,args, cmd, client, Discord){
+        //Code runs here. . . 
+        if(message.member.roles.cache.has('933401850059837450')){
 
-        if (!message.member.hasPermission("MANAGE_MESSAGES")) {
-            return message.reply("You don't have permission to do that.");
-        }
-        if (!arguments[0]) return message.reply("Please enter the amount of message you would like to clear.");
-        if (isNaN(arguments[0])) return message.reply("That is not a number.");
-
-        if (arguments[0] > 100) return message.reply("You cannot delete more than 100 messages.");
-        if (arguments[0] < 1) return message.reply("You must delete atleast 1 message.")
-
-        await message.channel.messages.fetch({ limit: arguments[0] }).then(messages => {
-            message.channel.bulkDelete(messages);
-        });
         const reply_message = new Discord.MessageEmbed()
         .setColor('#6d0da4')
         .setTitle('Successfully deleted messages!')
 
-        if(!args[0]) return message.reply("Please enter an amount of messages you wish to clear!");
-        if(isNaN(args[0])) return message.reply("Please enter a real number bro!");
+        if(!args[0]) return message.reply("``Please enter an amount of messages you wish to clear!``");
+        if(isNaN(args[0])) return message.reply("``Please enter a real number bro!``");
 
-        if(args[0] > 100) return message.reply("You cannot delete more than 100 messages!");
-        if(args[0] < 1) return message.reply("You must delete at least one message!");
+        if(args[0] > 100) return message.reply("``You cannot delete more than 100 messages!``");
+        if(args[0] < 1) return message.reply("``You must delete at least one message!``");
 
         await message.channel.messages.fetch({limit: args[0]}).then(messages =>{ // Fetches messages
             message.channel.bulkDelete(messages); // Deletes messages
@@ -36,7 +26,4 @@ module.exports = {
         });
     }
  }
-
-
-    
-
+}
