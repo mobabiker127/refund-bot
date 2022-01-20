4b425d178235b4631e443c3caa177804f93ccd19
Blactose <87344571+Blactose@users.noreply.github.com>
2022-01-20 16:23:57 +0000

    Refund399

--- commands/clear.js
+++ commands/clear.js
@@ -8,18 +8,20 @@ module.exports = {
     permissions: ['MANAGE_MESSAGES'],
     async callback(message, arguments, text) {
 
-        if (!message.member.hasPermission("MANAGE_MESSAGES")) {
-            return message.reply("You don't have permission to do that.");
-        }
-        if (!arguments[0]) return message.reply("Please enter the amount of message you would like to clear.");
-        if (isNaN(arguments[0])) return message.reply("That is not a number.");
+        const reply_message = new Discord.MessageEmbed()
+        .setColor('#6d0da4')
+        .setTitle('Successfully deleted messages!')
 
-        if (arguments[0] > 100) return message.reply("You cannot delete more than 100 messages.");
-        if (arguments[0] < 1) return message.reply("You must delete atleast 1 message.")
+        if(!args[0]) return message.reply("Please enter an amount of messages you wish to clear!");
+        if(isNaN(args[0])) return message.reply("Please enter a real number bro!");
 
-        await message.channel.messages.fetch({ limit: arguments[0] }).then(messages => {
-            message.channel.bulkDelete(messages);
+        if(args[0] > 100) return message.reply("You cannot delete more than 100 messages!");
+        if(args[0] < 1) return message.reply("You must delete at least one message!");
+
+        await message.channel.messages.fetch({limit: args[0]}).then(messages =>{ // Fetches messages
+            message.channel.bulkDelete(messages); // Deletes messages
+            message.channel.send(reply_message)
         });
-
     }
-}
+ }
+
