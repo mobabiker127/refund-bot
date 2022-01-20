52bb4f280289cf07263cda5fef882ee3ac083b49
Blactose <87344571+Blactose@users.noreply.github.com>
2022-01-20 16:28:52 +0000

    Refund400

--- commands/clear.js
+++ commands/clear.js
@@ -8,6 +8,18 @@ module.exports = {
     permissions: ['MANAGE_MESSAGES'],
     async callback(message, arguments, text) {
 
+        if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+            return message.reply("You don't have permission to do that.");
+        }
+        if (!arguments[0]) return message.reply("Please enter the amount of message you would like to clear.");
+        if (isNaN(arguments[0])) return message.reply("That is not a number.");
+
+        if (arguments[0] > 100) return message.reply("You cannot delete more than 100 messages.");
+        if (arguments[0] < 1) return message.reply("You must delete atleast 1 message.")
+
+        await message.channel.messages.fetch({ limit: arguments[0] }).then(messages => {
+            message.channel.bulkDelete(messages);
+        });
         const reply_message = new Discord.MessageEmbed()
         .setColor('#6d0da4')
         .setTitle('Successfully deleted messages!')
@@ -24,4 +36,7 @@ module.exports = {
         });
     }
  }
+
+
+    
 
