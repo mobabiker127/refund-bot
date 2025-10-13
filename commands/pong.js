6de25926314221b8f2307e1af99e6f2d41e83649
Mo Babiker <mobabiker127@gmail.com>
2025-10-13 16:58:30 +0100

    Testing slash command

--- commands/pong.js
+++ commands/pong.js
@@ -1,6 +1,7 @@
-const { Client, Events, SlashCommandBuilder } = require('discord.js');
 
-const client = new Client({ intents: [] });
+const Discord = require('discord.js');
+
+const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
 
 client.on(Events.InteractionCreate, interaction => {
     if (interaction.commandname === "ping") {
