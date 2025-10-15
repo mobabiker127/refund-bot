31aaf5aa2fc6ee9f12d9e9c902ec92d30c9da953
Mo Babiker <mobabiker127@gmail.com>
2025-10-15 20:54:53 +0100

    Updated ping slash command

--- slash-commands/ping.js
+++ slash-commands/ping.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('pingg')
+    .setName('ping')
     .setDescription('Replies with Pong!'),
     
   async execute(interaction) {
